9aba4b5aa1c77afcb2cae02858d69e848e636788
lakshit singhal <lakshitsinghal@lakshits-MacBook-Air.local>
2020-10-07 18:31:10 -0700

    Second upload

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -1,26 +1,55 @@
 package cse360assignment02;
 
-public class AddingMachine {
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddingMachine
+{
     private int total;
+    private List<String> arr = new ArrayList<>();
 
-    public AddingMachine () {
+    public AddingMachine ()
+    {
         total = 0;  // not needed - included for clarity
+        String temp = total+ "";
+        arr.add(temp);
     }
 
-    public int getTotal () {
-        return 0;
+    public int getTotal ()
+    {
+       return this.total;
     }
 
-    public void add (int value) {
+    public void add (int value)
+    {
+        String temp = " + "+value;
+        arr.add(temp);
+        this.total += value; // to add
     }
 
-    public void subtract (int value) {
+    public void subtract (int value)
+    {
+        String temp = " - "+value;
+        arr.add(temp);
+        this.total -= value; // to subtract
     }
 
-    public String toString () {
-        return "";
+    public String toString ()
+    {
+        String result = " ";
+        for (String s: arr)
+        {
+            result += s;
+        }
+        return result ;
     }
 
-    public void clear() {
+    public void clear()
+    {
+        arr.clear();
+        this.total = 0;
+        String temp = 0+"";
+        arr.add(temp);  // clear method
     }
+    
 }
